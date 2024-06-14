77e795745d525f5e8bf1d9aaf20b95ed214cf64a
Raya Gerdzhikova <rgerdzhi@car10s3.42barcelona.com>
2024-06-14 17:21:36 +0200

    correcccion

--- ex26/ft_count_if.c
+++ ex26/ft_count_if.c
@@ -6,7 +6,7 @@
 /*   By: rgerdzhi <rgerdzhi@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/14 15:21:17 by rgerdzhi          #+#    #+#             */
-/*   Updated: 2024/06/14 15:29:20 by rgerdzhi         ###   ########.fr       */
+/*   Updated: 2024/06/14 17:18:38 by rgerdzhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	ft_count_if(char **tab, int (*f)(char*))
 
 	i = 0;
 	counter = 0;
-	while (tab[i] != '\0')
+	while (tab[i])
 	{
 		if (f(tab[i]) == 1)
 		{
